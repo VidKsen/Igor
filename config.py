53fc3ef156277ced459890dbf747c9b0e8b06bd7
VidKsen <57181839+VidKsen@users.noreply.github.com>
2021-01-16 22:11:46 +0300

    Add files via upload
    Файл с конфигурацией и скрипт запуска приложения (запуск приложения по команде flask run)

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'PiZDec'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATBASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
